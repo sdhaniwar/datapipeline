042c1da33bf3634c7794d7a13900b5ed80b1a54c
Your Name <you@example.com>
2020-03-08 23:12:17 +0530

    database update

--- dataingestion.py
+++ dataingestion.py
@@ -22,10 +22,10 @@ def run(argv= None):
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--input', dest='input',required=False,help='Input file is read from local or ',default= 'gs://myhello/result1.csv')
+    parser.add_argument('--input', dest='input',required=False,help='Input file is read from local or cloud',default= 'gs://myhello/result1.csv')
 
 
-    parser.add_argument('--output', dest= 'output', required=False, help='output BQ table to write results to',default='new1.bigquerydemo')
+    parser.add_argument('--output', dest= 'output', required=False, help='output BQ table to write results to',default='new1.gcpemp')
 
     dataingestion = Dataingestion()
 
