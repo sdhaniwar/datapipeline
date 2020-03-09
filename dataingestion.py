266c0ae67d8f6bec4766ac2db8c5e5c0d7141c02
Your Name <you@example.com>
2020-03-09 12:01:20 +0530

    runner

--- dataingestion.py
+++ dataingestion.py
@@ -29,7 +29,7 @@ def run(argv= None):
 
     dataingestion = Dataingestion()
 
-    args, pipelineknown = parser.parse_known_args(argv)
+    args, pipelineknown = parser.parse_known_args(argv, runner = True)
     p = beam.Pipeline(options=PipelineOptions(pipelineknown))
     (p
 
@@ -40,7 +40,7 @@ def run(argv= None):
     |'Write to Bigquery' >> beam.io.Write(beam.io.WriteToBigQuery(args.output , schema= 'id : STRING,name : STRING,date : STRING, user_id : STRING,class : STRING,tag_based : STRING',
     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
     write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE)))
-    p.run().wait_until_finish(runner = True)
+    p.run().wait_until_finish()
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
