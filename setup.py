e0748cf4b2fc48e64aaf52872daad41732d5e3ef
Your Name <you@example.com>
2020-03-09 13:13:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='datapipeline',
+      version='1.0',
+      description = 'Dataflow in GCP',
+      url = 'https://github.com/sdhaniwar/datapipeline',
+      packages = ['datapipeline'],
+      )
