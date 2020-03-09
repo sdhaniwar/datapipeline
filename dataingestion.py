f80da70f982c2cbb66e0ec0544226fb15cb30f6d
Your Name <you@example.com>
2020-03-09 19:33:37 +0530

    new data

--- dataingestion.py
+++ dataingestion.py
@@ -8,7 +8,6 @@ import apache_beam as beam
 
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io import BigQuerySource
-from apache_beam.io import BigQueryDisposition
 
 class Dataingestion():
     def parse_method(self,strinput):
@@ -26,7 +25,7 @@ def run(argv= None):
     parser.add_argument('--input', dest='input',required=False,help='Input file is read from local or',default= 'gs://mydstore/result1.csv')
 
 
-    parser.add_argument('--output', dest= 'output', required=False, help='output BQ table to write results to',default='emp.empdata')
+    parser.add_argument('--output', dest= 'output', required=False, help='output BQ table to write results to',default='dataingcp:emp.empdata')
 
 
     known_args, pipeline_args = parser.parse_known_args(argv)
