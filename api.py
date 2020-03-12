d01b85343ffe3cfb57e85fb373621fa0cd16baa2
Your Name <you@example.com>
2020-03-12 23:24:03 +0530

    post

--- api.py
+++ api.py
@@ -6,14 +6,17 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'dataflow'
-app.config['MONGO_COLLECTIONNAME'] = 'userdata'
 app.config['MONGO_URI'] = 'mongodb+srv://sdhaniwar:<sumeet1508>@cluster0-c00d8.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
-@app.route('/')
+@app.route('/', methods= ['POST'])
 def home():
-    return 'This is the homepage'
+    dataflow = mongo.db.dataflow.userdata
+    name = request.json['name']
+    distance = request.json['distance']
+    dataflow_id = dataflow.insert({'name': name, 'distance': distance})
+    return render_template("index.html","Successfull")
 
 @app.route('/uploads/addata', methods=['POST'])
 def adddata():
