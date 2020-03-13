3160bb1e9ada0b11dc8d203d6afb977c9938ee5d
Your Name <you@example.com>
2020-03-13 11:33:29 +0530

    new changes

--- api.py
+++ api.py
@@ -2,29 +2,43 @@ from flask import Flask, render_template
 from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
 
+client = MongoClient("mongodb+srv://sdhaniwar:sumeet1508@cluster0-c00d8.gcp.mongodb.net/test?retryWrites=true&w=majority")
+mydb = client['dataflow']
+mycol = client['mycol']
+db = client.dataflow
+
 app.config['MONGO_DBNAME'] = 'dataflow'
-app.config['MONGO_URI'] = 'mongodb+srv://sdhaniwar:<sumeet1508>@cluster0-c00d8.gcp.mongodb.net/test?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://sdhaniwar:sumeet1508@cluster0-c00d8.gcp.mongodb.net/test?retryWrites=true&w=majority'
+serverstatus = db.command("serverStatus")
+print(serverstatus)
 
 mongo = PyMongo(app)
 
-@app.route('/', methods= ['POST'])
-def home():
-    dataflow = mongo.db.dataflow.userdata
-    name = request.json['name']
-    distance = request.json['distance']
-    dataflow_id = dataflow.insert({'name': name, 'distance': distance})
-    return render_template("index.html","Successfull")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails['name']
+        email = userDetails['email']
+        mydict = {'name': name , 'email': email}
+        x = db.mycol.insert_one(mydict)
 
-@app.route('/uploads/addata', methods=['POST'])
-def adddata():
-  dataflow = mongo.db.dataflow.userdata
-  name = request.json['name']
-  distance = request.json['distance']
-  dataflow_id = dataflow.insert({'name': name, 'distance': distance})
-  return render_template("index.html")
+    return render_template("index.html")
+@app.route('/result', methods= ['GET'])
+def result():
+  datas = db.mycol.find()
+  resp = []
+  for data in datas:
+    data['_id'] = str(data['_id'])
+    resp.append(data)
+  return json.dumps(resp)  
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- gettingapi.py
+++ gettingapi.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-response = requests.get("http://127.0.0.1:5000/todo/api/tasks")
+response = requests.get("http://127.0.0.1:5000/")
 
 def jprint(obj):
     #to create a python object
